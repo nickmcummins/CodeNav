84a158b6eabf2dc12fcfd311e867536ec49e5b2b
Nick M Cummins <nickmcummins@gmail.com>
2022-07-13 19:55:35 -0700

    initial work for BaseMapperXML.MapBase to match paradigm

--- Languages/XML/Mappers/BaseMapperXML.cs
+++ Languages/XML/Mappers/BaseMapperXML.cs
@@ -1,11 +1,48 @@
-﻿using Microsoft.Language.Xml;
+﻿using CodeNav.Helpers;
+using CodeNav.Mappers;
+using CodeNav.Models;
+using Microsoft.Language.Xml;
+using Microsoft.VisualStudio.Imaging;
+using System;
 using System.Linq;
+using System.Windows.Media;
+using System.Xml;
 using static CodeNav.Constants;
 
 namespace CodeNav.Languages.XML.Mappers
 {
     public static class BaseMapperXML
     {
+        public static T MapBase<T>(string sourceString, XmlElementSyntax xmlElement, ICodeViewUserControl? control) where T : CodeItem
+        {
+            var element = Activator.CreateInstance<T>();
+
+            element.Name = GetFullName(xmlElement);
+            element.FullName = element.Name;
+            element.Id = element.Name;
+            element.Tooltip = element.Name;
+            element.StartLine = GetLineNumber(sourceString, xmlElement.Span.Start);
+            element.StartLinePosition = new Microsoft.CodeAnalysis.Text.LinePosition(element.StartLine.GetValueOrDefault() - 1, 0);
+            element.EndLine = GetLineNumber(sourceString, xmlElement.Span.End);
+            element.EndLinePosition = new Microsoft.CodeAnalysis.Text.LinePosition(element.EndLine.GetValueOrDefault(), 0);
+            element.ForegroundColor = Colors.Black;
+
+            element.Access = CodeItemAccessEnum.Public;
+            element.Control = control;
+            element.Span = new Microsoft.CodeAnalysis.Text.TextSpan(xmlElement.Span.Start, xmlElement.Span.End - xmlElement.Span.Start);
+            element.Moniker = KnownMonikers.XMLElement;
+            element.FontSize = SettingsHelper.Font.SizeInPoints;
+            element.ParameterFontSize = SettingsHelper.Font.SizeInPoints - 1;
+            element.FontFamily = new FontFamily(SettingsHelper.Font.FontFamily.Name);
+            element.FontStyle = FontStyleMapper.Map(SettingsHelper.Font.Style);
+            element.Kind = CodeItemKindEnum.Property;
+
+            element.FilePath = control?.CodeDocumentViewModel.FilePath ?? string.Empty;
+
+            return element;
+
+        }
+
         public static string GetFullName(IXmlElement xmlElement)
         {
             var attributes = xmlElement.Attributes.Any() ? " " + string.Join(" ", xmlElement.Attributes.Select(attr => $"{attr.Key}={DoubleQuote}{attr.Value}{DoubleQuote}")) : string.Empty;

bca76efe65be74cbe5e75dc1b4958dcb98a32ef8
Nick M Cummins <nickmcummins@gmail.com>
2022-08-10 21:04:21 -0700

    improve performance of IconMapper by using direct switch/if conditions

--- Mappers/IconMapper.cs
+++ Mappers/IconMapper.cs
@@ -1,98 +1,126 @@
 ﻿using CodeNav.Models;
-using Microsoft.VisualStudio.Imaging;
 using Microsoft.VisualStudio.Imaging.Interop;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
+using static CodeNav.Models.CodeItemAccessEnum;
+using static Microsoft.VisualStudio.Imaging.KnownMonikers;
 
 namespace CodeNav.Mappers
 {
     public static class IconMapper
     {
-        public static readonly IDictionary<string, ImageMoniker> MonikersByName = typeof(KnownMonikers).GetProperties().ToDictionary(m => m.Name, m => (ImageMoniker)m.GetValue(null, null));
-
         public static ImageMoniker MapMoniker(CodeItemKindEnum kind, CodeItemAccessEnum access)
         {
-            string monikerString;
-            var accessString = GetEnumDescription(access);
-
             switch (kind)
             {
                 case CodeItemKindEnum.Namespace:
-                    monikerString = $"Namespace{accessString}";
-                    break;
+                    if (access == Public) return NamespacePublic;
+                    else if (access == Private) return NamespacePrivate;
+                    else if (access == Protected) return NamespaceProtected;
+                    else if (access == CodeItemAccessEnum.Internal) return NamespaceInternal;
+                    else if (access == Sealed) return NamespaceSealed;
+                    else return Namespace;
                 case CodeItemKindEnum.Class:
-                    monikerString = $"Class{accessString}";
-                    break;
+                    if (access == Public) return ClassPublic;
+                    else if (access == Private) return ClassPrivate;
+                    else if (access == Protected) return ClassProtected;
+                    else if (access == CodeItemAccessEnum.Internal) return ClassInternal;
+                    else if (access == Sealed) return ClassSealed;
+                    else return Class;
                 case CodeItemKindEnum.Constant:
-                    monikerString = $"Constant{accessString}";
-                    break;
+                    if (access == Public) return ConstantPublic;
+                    else if (access == Private) return ConstantPrivate;
+                    else if (access == Protected) return ConstantProtected;
+                    else if (access == CodeItemAccessEnum.Internal) return ConstantInternal;
+                    else if (access == Sealed) return ConstantSealed;
+                    else return Constant;
                 case CodeItemKindEnum.Delegate:
-                    monikerString = $"Delegate{accessString}";
-                    break;
+                    if (access == Public) return DelegatePublic;
+                    else if (access == Private) return DelegatePrivate;
+                    else if (access == Protected) return DelegateProtected;
+                    else if (access == CodeItemAccessEnum.Internal) return DelegateInternal;
+                    else if (access == Sealed) return DelegateSealed;
+                    else return Delegate; break;
                 case CodeItemKindEnum.Enum:
-                    monikerString = $"Enumeration{accessString}";
-                    break;
+                    if (access == Public) return EnumerationPublic;
+                    else if (access == Private) return EnumerationPrivate;
+                    else if (access == Protected) return EnumerationProtected;
+                    else if (access == CodeItemAccessEnum.Internal) return EnumerationInternal;
+                    else if (access == Sealed) return EnumerationSealed;
+                    else return Enumeration;
                 case CodeItemKindEnum.EnumMember:
-                    monikerString = $"EnumerationItem{accessString}";
-                    break;
+                    if (access == Public) return EnumerationItemPublic;
+                    else if (access == Private) return EnumerationItemPrivate;
+                    else if (access == Protected) return EnumerationItemProtected;
+                    else if (access == CodeItemAccessEnum.Internal) return EnumerationItemInternal;
+                    else if (access == Sealed) return EnumerationItemSealed;
+                    else return EnumerationItemPublic;
                 case CodeItemKindEnum.Event:
-                    monikerString = $"Event{accessString}";
-                    break;
+                    if (access == Public) return EventPublic;
+                    else if (access == Private) return EventPrivate;
+                    else if (access == Protected) return EventProtected;
+                    else if (access == CodeItemAccessEnum.Internal) return EventInternal;
+                    else if (access == Sealed) return EventSealed;
+                    else return Event;
                 case CodeItemKindEnum.Interface:
-                    monikerString = $"Interface{accessString}";
-                    break;
+                    if (access == Public) return InterfacePublic;
+                    else if (access == Private) return InterfacePrivate;
+                    else if (access == Protected) return InterfaceProtected;
+                    else if (access == CodeItemAccessEnum.Internal) return InterfaceInternal;
+                    else if (access == Sealed) return InterfaceSealed;
+                    else return Interface;
                 case CodeItemKindEnum.Constructor:
                 case CodeItemKindEnum.Method:
-                    monikerString = $"Method{accessString}";
+                    if (access == Public) return MethodPublic;
+                    else if (access == Private) return MethodPrivate;
+                    else if (access == Protected) return MethodProtected;
+                    else if (access == CodeItemAccessEnum.Internal) return MethodInternal;
+                    else if (access == Sealed) return MethodSealed;
+                    else return Method;
                     break;
                 case CodeItemKindEnum.Property:
                 case CodeItemKindEnum.Indexer:
-                    monikerString = $"Property{accessString}";
-                    break;
+                    if (access == Public) return PropertyPublic;
+                    else if (access == Private) return PropertyPrivate;
+                    else if (access == Protected) return PropertyProtected;
+                    else if (access == CodeItemAccessEnum.Internal) return PropertyInternal;
+                    else if (access == Sealed) return PropertySealed;
+                    else return Property;
                 case CodeItemKindEnum.Struct:
                 case CodeItemKindEnum.Record:
-                    monikerString = $"Structure{accessString}";
-                    break;
+                    if (access == Public) return StructurePublic;
+                    else if (access == Private) return StructurePrivate;
+                    else if (access == Protected) return StructureProtected;
+                    else if (access == CodeItemAccessEnum.Internal) return StructureInternal;
+                    else if (access == Sealed) return StructureSealed;
+                    else return Structure;
                 case CodeItemKindEnum.Variable:
-                    monikerString = $"Field{accessString}";
-                    break;
+                    if (access == Public) return FieldPublic;
+                    else if (access == Private) return FieldPrivate;
+                    else if (access == Protected) return FieldProtected;
+                    else if (access == CodeItemAccessEnum.Internal) return FieldInternal;
+                    else if (access == Sealed) return FieldSealed;
+                    else return Field;
                 case CodeItemKindEnum.Switch:
-                    monikerString = "FlowSwitch";
-                    break;
+                    return FlowSwitch;
                 case CodeItemKindEnum.SwitchSection:
-                    monikerString = "FlowDecision";
-                    break;
+                    return FlowDecision;
                 case CodeItemKindEnum.StyleRule:
-                    monikerString = "Rule";
-                    break;
+                    return Rule;
                 case CodeItemKindEnum.PageRule:
-                    monikerString = "PageStyle";
-                    break;
+                    return PageStyle;
                 case CodeItemKindEnum.NamespaceRule:
-                    monikerString = "Namespace";
-                    break;
+                    return Namespace;
                 case CodeItemKindEnum.MediaRule:
-                    monikerString = "Media";
-                    break;
+                    return Media;
                 case CodeItemKindEnum.FontFaceRule:
-                    monikerString = "Font";
-                    break;
+                    return Font;
                 default:
-                    monikerString = $"Property{accessString}";
-                    break;
+                    if (access == Public) return PropertyPublic;
+                    else if (access == Private) return PropertyPrivate;
+                    else if (access == Protected) return PropertyProtected;
+                    else if (access == CodeItemAccessEnum.Internal) return PropertyInternal;
+                    else if (access == Sealed) return PropertySealed;
+                    else return Property;
             }
-
-            return MonikersByName.TryGetValue(monikerString, out var moniker) ? moniker : KnownMonikers.QuestionMark;
-
-        }
-
-        private static string GetEnumDescription(this Enum value)
-        {
-            var field = value.GetType().GetField(value.ToString());
-            var attribute = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
-            return attribute == null ? value.ToString() : attribute.Description;
         }
     }
 }
